65193da95c5828f5c8492db34cbd2189aa77fa68
Vanshika-Rana <ranavanshika172000@gmail.com>
2026-05-31 23:29:00 +0530

    room numbers 101-105 + cache bust

--- sw.js
+++ sw.js
@@ -1,59 +1,69 @@
-const CACHE_NAME = "manodaya-v3";
+const CACHE_NAME = "manodaya-v4";
 
 const STATIC_ASSETS = [
-  "./index.html",
-  "./style.css",
-  "./app.js",
-  "./config.js",
-  "./manifest.json",
-  "./icons/icon-192.png",
-  "./icons/icon-512.png",
+	"./index.html",
+	"./style.css",
+	"./app.js",
+	"./config.js",
+	"./manifest.json",
+	"./icons/icon-192.png",
+	"./icons/icon-512.png",
 ];
 
 const NETWORK_FIRST = ["/config.js", "/app.js", "/style.css", "/index.html"];
 
 self.addEventListener("install", (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(STATIC_ASSETS))
-  );
-  self.skipWaiting();
+	event.waitUntil(
+		caches.open(CACHE_NAME).then((cache) => cache.addAll(STATIC_ASSETS)),
+	);
+	self.skipWaiting();
 });
 
 self.addEventListener("activate", (event) => {
-  event.waitUntil(
-    caches.keys().then((keys) =>
-      Promise.all(keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key)))
-    )
-  );
-  self.clients.claim();
+	event.waitUntil(
+		caches
+			.keys()
+			.then((keys) =>
+				Promise.all(
+					keys
+						.filter((key) => key !== CACHE_NAME)
+						.map((key) => caches.delete(key)),
+				),
+			),
+	);
+	self.clients.claim();
 });
 
 function isNetworkFirst(url) {
-  return NETWORK_FIRST.some((path) => url.pathname.endsWith(path));
+	return NETWORK_FIRST.some((path) => url.pathname.endsWith(path));
 }
 
 self.addEventListener("fetch", (event) => {
-  if (event.request.method !== "GET") return;
+	if (event.request.method !== "GET") return;
 
-  const url = new URL(event.request.url);
-  if (url.origin !== self.location.origin) return;
+	const url = new URL(event.request.url);
+	if (url.origin !== self.location.origin) return;
 
-  if (isNetworkFirst(url)) {
-    event.respondWith(
-      fetch(event.request)
-        .then((response) => {
-          if (response.ok) {
-            const clone = response.clone();
-            caches.open(CACHE_NAME).then((cache) => cache.put(event.request, clone));
-          }
-          return response;
-        })
-        .catch(() => caches.match(event.request))
-    );
-    return;
-  }
+	if (isNetworkFirst(url)) {
+		event.respondWith(
+			fetch(event.request)
+				.then((response) => {
+					if (response.ok) {
+						const clone = response.clone();
+						caches
+							.open(CACHE_NAME)
+							.then((cache) => cache.put(event.request, clone));
+					}
+					return response;
+				})
+				.catch(() => caches.match(event.request)),
+		);
+		return;
+	}
 
-  event.respondWith(
-    caches.match(event.request).then((cached) => cached || fetch(event.request))
-  );
+	event.respondWith(
+		caches
+			.match(event.request)
+			.then((cached) => cached || fetch(event.request)),
+	);
 });
