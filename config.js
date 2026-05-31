268ffad98ae770f667436a8a741ec59fa39a4bd1
Vanshika-Rana <ranavanshika172000@gmail.com>
2026-05-31 23:25:16 +0530

    fix room numbers and encoding

--- config.js
+++ config.js
@@ -1,5 +1,6 @@
 const CONFIG = {
-  SHEET_URL: "https://script.google.com/macros/s/AKfycbwSBbKk81_tYiPQgwZy_nGEVunoGeYfDHsgI1e1Tbrz7tcCJ-QZfKSRkXkx5BN6zZ-D/exec",
-  ROOMS: ["Room 1", "Room 2", "Room 3", "Room 4", "Room 5"],
-  BOOKERS: ["Virender Rana", "Rakesh Rana"],
+	SHEET_URL:
+		"https://script.google.com/macros/s/AKfycbwSBbKk81_tYiPQgwZy_nGEVunoGeYfDHsgI1e1Tbrz7tcCJ-QZfKSRkXkx5BN6zZ-D/exec",
+	ROOMS: ["Room 101", "Room 102", "Room 103", "Room 104", "Room 105"],
+	BOOKERS: ["Virender Rana", "Rakesh Rana"],
 };
